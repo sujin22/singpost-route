537dfe1fcc8d1d4f85252706dfc1e0565a146a9d
sujin22 <aoghksj@naver.com>
2023-05-17 18:11:59 +0900

    build: new version

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/singpost-route/",
   server: {
     hmr:
       process.env.CODESANDBOX_SSE || process.env.GITPOD_WORKSPACE_ID
